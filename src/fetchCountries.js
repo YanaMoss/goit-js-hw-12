637bcea2a9aa6fc9c7ad0a0208c379d1b622ec0f
Yana Sokurenko <yanamoss@gmail.com>
2021-07-25 18:07:15 +0300

    HW-12 -done

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,7 +1,9 @@
 'use strict';
 export const fetchCountries = (name) => {
    const baseUrl = `https://restcountries.eu/rest/v2/name/${name}?fields=name;capital;population;flag;languages`;
-   console.log(baseUrl);
+   if (name.trim() === '') {
+      return Promise.resolve([]);
+   };
    return fetch(baseUrl)
       .then(response => {
          return response.json();
